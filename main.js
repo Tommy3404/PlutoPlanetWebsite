6f265a77f984a34bf4261a42c911e06679a442e9
Tommy McClure <tommy.mcclure@fhnw.edu>
2026-03-05 21:39:35 -0700

    added resources, moons, timeline, and intro

--- main.js
+++ main.js
@@ -25,3 +25,19 @@ const gridItems = facts.map(fact => {
 const completeHTML = gridItems.join('');
 gridContainer.innerHTML = completeHTML;
 
+
+document.querySelectorAll(".clickable").forEach(function(img) {
+
+    img.addEventListener("click", function() {
+
+        const text = img.nextElementSibling;
+
+        if (text.style.display === "block") {
+            text.style.display = "none";
+        } else {
+            text.style.display = "block";
+        }
+
+    });
+
+});
